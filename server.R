e403bdde336aa567f04238838bd7016e92bec3a3
Dave Campbell <iamdavecampbell@users.noreply.github.com>
2021-01-27 22:34:58 -0500

    final construction

--- server.R
+++ server.R
@@ -15,7 +15,6 @@ shinyServer(function(input, output) {
         N = 1000 # number of fish to start
         T = input$generations # generaions
         FishSizeSD = 1        #sd for fish
-        NetSize = 6           #fish this size or larger get caught if they encounter the net
         #512 is the default number of increments for density
         FishDensityThatWeKeep = matrix(NA, nrow=512,ncol=T, dimnames = list(NULL, paste("Gen",1:T,sep=".")))
         LivingFishSize = rnorm(N, mean = 6, sd = FishSizeSD)# in lbs, size of current fish distribution.
@@ -24,9 +23,9 @@ shinyServer(function(input, output) {
         for(generation in 2:T){
 
             EncounterNet = runif(N)<input$P.net.encounter ###### FROM INPUT
-            SurviveIfInNet = LivingFishSize<NetSize
+            SurviveIfInNet = LivingFishSize<input$NetSize         #fish this size or larger get caught if they encounter the net
             Fish.At.Last.Gen = data.frame(LivingFishSize, EncounterNet, SurviveIfInNet)
-            Fish.Parents.For.Next.Gen = Fish.At.Last.Gen%>% filter(EncounterNet==FALSE | (EncounterNet ==TRUE & SurviveIfInNet ==TRUE))%>%
+            Fish.Parents.For.Next.Gen = Fish.At.Last.Gen%>% filter(EncounterNet==FALSE | (EncounterNet ==TRUE & SurviveIfInNet ==TRUE) & LivingFishSize>0)%>%
                                         dplyr::select(LivingFishSize)
             #this ensures that only survivors can reproduce and that the population stays constant in count.
             Fish.Parents.For.Next.Gen.Size = Fish.Parents.For.Next.Gen[sample(1:nrow(Fish.Parents.For.Next.Gen),size=N,replace=TRUE) ,1]
@@ -39,8 +38,11 @@ shinyServer(function(input, output) {
         FishDensityThatWeKeep %>% gather(key = Generation,
                                                 value = DensityOfMass,
                                                 -fishmass) %>%
+            mutate(Generation = as.numeric(gsub(Generation,pattern = "Gen.",replacement = "")))%>%
         ggplot( aes(x = fishmass, y = DensityOfMass, colour = Generation )) +
-            geom_point()
+            geom_point()+
+            geom_vline(xintercept = input$NetSize, lwd=3,col="red")+
+            ggtitle("Distribution of salmon weight in each generation after responding to Gill-net pressure")
 
     })
 
