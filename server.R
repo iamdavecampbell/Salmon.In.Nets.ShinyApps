29c9b5af9100deebe5ad2b05048b4d8ec04548e4
Dave Campbell <iamdavecampbell@users.noreply.github.com>
2021-01-27 22:29:45 -0500

    Stage 2 iteration

--- server.R
+++ server.R
@@ -15,6 +15,7 @@ shinyServer(function(input, output) {
         N = 1000 # number of fish to start
         T = input$generations # generaions
         FishSizeSD = 1        #sd for fish
+        NetSize = 6           #fish this size or larger get caught if they encounter the net
         #512 is the default number of increments for density
         FishDensityThatWeKeep = matrix(NA, nrow=512,ncol=T, dimnames = list(NULL, paste("Gen",1:T,sep=".")))
         LivingFishSize = rnorm(N, mean = 6, sd = FishSizeSD)# in lbs, size of current fish distribution.
@@ -23,9 +24,9 @@ shinyServer(function(input, output) {
         for(generation in 2:T){
 
             EncounterNet = runif(N)<input$P.net.encounter ###### FROM INPUT
-            SurviveIfInNet = LivingFishSize<input$NetSize         #fish this size or larger get caught if they encounter the net
+            SurviveIfInNet = LivingFishSize<NetSize
             Fish.At.Last.Gen = data.frame(LivingFishSize, EncounterNet, SurviveIfInNet)
-            Fish.Parents.For.Next.Gen = Fish.At.Last.Gen%>% filter(EncounterNet==FALSE | (EncounterNet ==TRUE & SurviveIfInNet ==TRUE) & LivingFishSize>0)%>%
+            Fish.Parents.For.Next.Gen = Fish.At.Last.Gen%>% filter(EncounterNet==FALSE | (EncounterNet ==TRUE & SurviveIfInNet ==TRUE))%>%
                                         dplyr::select(LivingFishSize)
             #this ensures that only survivors can reproduce and that the population stays constant in count.
             Fish.Parents.For.Next.Gen.Size = Fish.Parents.For.Next.Gen[sample(1:nrow(Fish.Parents.For.Next.Gen),size=N,replace=TRUE) ,1]
@@ -38,11 +39,8 @@ shinyServer(function(input, output) {
         FishDensityThatWeKeep %>% gather(key = Generation,
                                                 value = DensityOfMass,
                                                 -fishmass) %>%
-            mutate(Generation = as.numeric(gsub(Generation,pattern = "Gen.",replacement = "")))%>%
         ggplot( aes(x = fishmass, y = DensityOfMass, colour = Generation )) +
-            geom_point()+
-            geom_vline(xintercept = input$NetSize, lwd=3,col="red")+
-            ggtitle("Distribution of salmon weight in each generation after responding to Gill-net pressure")
+            geom_point()
 
     })
 
--- ui.R
+++ ui.R
@@ -18,40 +18,22 @@ shinyUI(fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            p("Salmon reproduce for this many generations:"),
             sliderInput("generations",
-                        "",
+                        "Generations Of Fish:",
                         min = 1,
-                        max = 100,
-                        value = 5),
-            p("The gill-net covers this proportion of the river:"),
+                        max = 20,
+                        value = 2),
             sliderInput("P.net.encounter",
-                        "",
+                        "Net coverage of the river:",
                         min = 0,
                         max = 1,
-                        value = .25),
-            p("The holes in the gill-net will catch all fish who encounter the net and are over this size (in lbs)"),
-            sliderInput("NetSize",
-                        "",
-                        min = 1,
-                        max = 10,
-                        value = 3)
+                        value = .01)
     ),
 
 
         # Show a plot of the generated distribution
         mainPanel(
-            p("Salmon, starting with an initial size distribution (darkest line below), encounter gill-nets across a river.  
-              Some fish will swim around the nets, but some fish encounter the nets.  
-              Fish bigger than the size of the holes in the net will get caught and will not survive to reproduce.
-              Smaller fish will swim through the holes in the net and will be able to reproduce.
-              Fish fry will be similar in size to their parents, but the size of the fry are random."),
-            plotOutput("distPlot"),
-            p("Notice how the distribution of fish sizes shrinks well below the gill-net size. Smaller fish are more successful against the nets."),
-            h3("limitations"),
-            p("Fish may have better survival rates in the ocean if they are larger, but the current model does not consider upwards pressure in size."),
-            
-            
+            plotOutput("distPlot")
         )
     )
 )
